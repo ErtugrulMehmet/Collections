c493f44f7dd77242aec62e99111f6cb76f3631e6
GreenBaret <mehmet_ertugrul64@hotmail.com>
2022-06-28 22:06:28 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> isimler = new List<string> { "mehmet", "sabriye" };
+            isimler.Add("mahmut");
+
+            MyList<string> isimler2 = new MyList<string>();
+
+
+
+                
+        }
+
+    }
+}
